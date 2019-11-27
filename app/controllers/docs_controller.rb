55133fbf40f4c34ce121fc87250c1005916b8ae6
daniel :) <hgpmac151@Thes-Air.hsd1.ca.comcast.net>
2019-11-27 09:33:17 -0800

    Association between User and Files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -9,11 +9,11 @@ def show
 end
 
 def new
-  @doc = Doc.new
+  @doc = current_user.docs.build
 end
 
 def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
 
     if @doc.save
       redirect_to @doc
@@ -27,7 +27,7 @@ end
 
 def update
   if @doc.update(doc_params)
-    redirect_to @doc 
+    redirect_to @doc
   else
     render 'edit'
   end
